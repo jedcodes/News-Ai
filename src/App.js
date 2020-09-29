410cb36d854abea7e9c85a95f91a5ab827b0be6a
jackdelamou <jackdelamou.97@gmail.com>
2020-09-29 08:58:43 +0200

    Adding finish project to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./Components/NewsCards/NewsCards";
+import useStyles from "./styles";
 
-function App() {
+const alanKey =
+  "787c1ef63d76b3edc7fd11145ab8689d2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const alanLogoSrc = "https://alan.app/voice/images/previews/preview.jpg";
+
+const App = () => {
+  const classes = useStyles();
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevArticle) => prevArticle + 1);
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className={classes.logoContainer}>
+        <img src={alanLogoSrc} alt="" className={classes.alanLogo} />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
-}
+};
 
 export default App;
